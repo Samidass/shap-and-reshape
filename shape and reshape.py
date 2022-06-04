74379c5a4b03b83e335302a04b26d2be6f53641d
Samidass <98681538+Samidass@users.noreply.github.com>
2022-06-04 08:35:11 +0530

    reshape array

---
+++ shape and reshape.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 08:33:53 2022
+
+@author: Sammy
+"""
+
+import numpy
+x=list(map(int,input().split()))
+my_array=numpy.array(x)
+print(numpy.reshape(my_array,(3,3)))
+
+
+'''1 2 3 4 5 6 7 8 9'''    
